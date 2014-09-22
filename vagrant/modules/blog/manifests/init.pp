d3b20b730a8b806904d900c31cdbd50220a46201
Nelson Senna <onelsonsenna@gmail.com>
2014-09-22 08:59:04 -0300

    Update of vm box and blog dependencies

--- vagrant/modules/blog/manifests/init.pp
+++ vagrant/modules/blog/manifests/init.pp
@@ -1,17 +1,29 @@
 class blog {
 
-    package { 'rubygems':
+    $packages = [
+        "make",
+        "ruby1.9.3",
+        "nodejs"
+    ]
+
+    package { $packages:
         ensure => present
+    }
+
+    exec { 'change_ruby_version':
+        command => 'sudo rm -f /usr/bin/ruby && sudo ln -s /usr/bin/ruby1.9.3 /usr/bin/ruby',
+        require => Package['ruby1.9.3']
     }
 
     exec { 'rdoc':
         command => 'sudo gem install rdoc',
-        require => Package['rubygems']
+        require => Package['ruby1.9.3']
     }
 
     exec { 'jekyll':
         command => 'sudo gem install jekyll',
-        require => Exec['rdoc']
+        require => Exec['rdoc'],
+        timeout => 0
     }
 
     exec { 'i18n-gem':
